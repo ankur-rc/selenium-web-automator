cc4a5e0671b724e392df1113a59433c3764a95a3
Ankur Roychowdhury <ankurrc.1993@gmail.com>
2016-02-06 21:15:56 +0530

    Removed configuration file from being tracked.

--- my-config.js
+++
@@ -1,9 +0,0 @@
-var TwilioCredentials = {
-    AUTH_SID: "ACa716fe706e57cdce3d47e5b253280b6d",
-    AUTH_TOKEN: "e441ce218ae0aa0c97dfcd8cbd082b1d",
-    NUMBER: "+1 802-267-3400"
-};
-
-module.exports = {
-    TwilioCredentials: TwilioCredentials
-};
