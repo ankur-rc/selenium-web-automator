f1a1c4a0e48fcb07317ebd25c9d34d5cea4007cb
Ankur Roychowdhury <ankurrc.1993@gmail.com>
2016-02-06 21:12:20 +0530

    Added initial working files.

---
+++ automator.js
@@ -0,0 +1,136 @@
+/**
+@author: Ankur Roy Chowdhury
+**/
+
+/////////////////////////////////////////////////////////////////////initialisation//////////////////////////////////////////////////////////////////////////////////////////////
+
+//constants
+const URL = 'http://www.delhi.gov.in/wps/wcm/connect/doit_transport/Transport/Home/Driving+Licence/Online+Appointment+and+Payment+for+Driving+License';
+const RECEIPT_NO = "HS-6837";
+
+//config
+var twilioCredentials = require("./my-config").TwilioCredentials;
+
+const EventEmitter = require('events');
+const util = require('util');
+
+//variables
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until,
+    driver = null,
+    last_result = null,
+    result = null;
+
+var twilio = require('twilio')(twilioCredentials.AUTH_SID, twilioCredentials.AUTH_TOKEN);
+
+/////////////////////////////////////////////////////////////////////event_emitter///////////////////////////////////////////////////////////////////////////////////////////////
+
+var EventEmitterImpl = function () {
+    EventEmitter.call(this);
+}
+
+util.inherits(EventEmitterImpl, EventEmitter);
+
+var eventEmitter = new EventEmitterImpl();
+
+//////////////////////////////////////////////////////////////////////functions//////////////////////////////////////////////////////////////////////////////////////////////////
+
+var executeAutomationScript = function () {
+
+    driver = new webdriver.Builder().forBrowser('firefox').build();
+    driver.get(URL);
+
+    driver.wait(until.elementLocated(By.xpath("html/body/table/tbody/tr/td/iframe")), 10000).then(
+        function (webElement) {
+            //console.log("Element found...Switching to iframe");
+            driver.switchTo().frame(0);
+        },
+        function (err) {
+            console.log("Element not found, exiting..." + err.message);
+        });
+
+    driver.wait(until.elementLocated(By.xpath(".//*[@id='ctl00_ContentPlaceHolder1_Image9']")), 5000).then(
+        function (webElement) {
+            //console.log("Element found...");
+            webElement.click();
+        },
+        function (err) {
+            console.log("Element not found, exiting..." + err.message);
+        });
+
+    driver.wait(until.elementLocated(By.xpath(".//*[@id='ctl00_ContentPlaceHolder1_ddlzone']/option[4]")), 5000).then(
+        function (webElement) {
+            //console.log("Element found...");
+            webElement.click();
+        },
+        function (err) {
+            console.log("Element not found, exiting..." + err.message);
+        });
+
+    driver.wait(until.elementLocated(By.xpath(".//*[@id='ctl00_ContentPlaceHolder1_TextBox1']")), 5000).then(
+        function (webElement) {
+            //console.log("Element found...");
+            webElement.sendKeys(RECEIPT_NO);
+        },
+        function (err) {
+            console.log("Element not found, exiting..." + err.message);
+        });
+
+    driver.wait(until.elementLocated(By.xpath(".//*[@id='ctl00_ContentPlaceHolder1_btnsearch1']")), 5000).then(
+        function (webElement) {
+            //console.log("Element found...");
+            webElement.click();
+        },
+        function (err) {
+            console.log("Element not found, exiting..." + err.message);
+        });
+
+    driver.wait(until.elementLocated(By.xpath(".//*[@id='ctl00_ContentPlaceHolder1_lblStatus']")), 5000).then(
+        function (element) {
+            element.getInnerHtml().then(
+                function (html) {
+                    console.log("Current Result: " + html.toString());
+                    var current_result = html.toString();
+                    eventEmitter.emit('result_changed', current_result);
+                    eventEmitter.emit('kill_driver');
+                });
+        });
+}
+
+var sendSms = function (text) {
+
+    console.log("Sending sms...");
+    twilio.sendMessage({
+
+        to: '+919999980842',
+        from: twilioCredentials.NUMBER,
+        body: text
+
+    }, function (err, responseData) {
+        if (!err) {
+            console.log(responseData.from);
+            console.log(responseData.body);
+        } else {
+            console.log(err.message);
+        }
+    });
+}
+
+//////////////////////////////////////////////////////////main_loop//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//setup event_emitters
+eventEmitter.on('result_changed', function (current_result) {
+    if (result !== current_result) {
+        result = current_result;
+        sendSms(result); //send sms 
+    }
+});
+
+eventEmitter.on('kill_driver', function () {
+    console.log("Killing driver instance...");
+    driver.quit();
+    setInterval(executeAutomationScript, 3600000);
+});
+
+executeAutomationScript();
---
+++ my-config.js
@@ -0,0 +1,9 @@
+var TwilioCredentials = {
+    AUTH_SID: "ACa716fe706e57cdce3d47e5b253280b6d",
+    AUTH_TOKEN: "e441ce218ae0aa0c97dfcd8cbd082b1d",
+    NUMBER: "+1 802-267-3400"
+};
+
+module.exports = {
+    TwilioCredentials: TwilioCredentials
+};
